148b71ff126c62a1e6e9db57c172423fb4b6eaa7
zinnli <limgx5192@gmail.com>
2023-05-20 14:57:55 +0900

    feat: input 컴포넌트 제작(#9)

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,6 +4,7 @@ import { useForm } from "react-hook-form";
 import styled, { css } from "styled-components";
 
 import { signup } from "apis";
+import { FormInput } from "components";
 
 const Register = () => {
   const navigate = useNavigate();
@@ -43,67 +44,58 @@ const Register = () => {
   return (
     <Root onSubmit={handleSubmit(handleRegister)}>
       <Title>회원가입</Title>
-      <RegisterInput>
-        <Label htmlFor="userId">아이디</Label>
-        <Input
-          type="text"
-          id="userId"
-          value={watch("userId")}
-          isError={!!errors.userId}
-          placeholder="아이디를 입력하세요"
-          {...register("userId", {
-            required: "아이디를 입력해주세요",
-            pattern: {
-              value: /^(?=.*[a-z0-9])[a-z0-9]{3,10}$/,
-              message: "아이디는 3-10글자의 영어와 숫자로 입력해주세요.",
-            },
-          })}
-        />
-        <ErrorMsg>
-          {errors.userId && "아이디는 3-10글자의 영어와 숫자로 입력해주세요."}
-        </ErrorMsg>
-      </RegisterInput>
-      <RegisterInput>
-        <Label htmlFor="password">비밀번호</Label>
-        <Input
-          type="password"
-          id="password"
-          value={watch("password")}
-          isError={!!errors.password}
-          placeholder="비밀번호를 입력하세요"
-          {...register("password", {
-            required: "비밀번호를 입력해주세요",
-            pattern: {
-              value: /^[a-zA-Z0-9!@#$%^&*()._-]{3,10}$/,
-              message: "비밀번호는 3-10글자로 입력해주세요.",
-            },
-          })}
-        />
-        <ErrorMsg>
-          {errors.password && "비밀번호는 3-10글자로 입력해주세요."}
-        </ErrorMsg>
-      </RegisterInput>
-      <RegisterInput>
-        <Label htmlFor="nickname">닉네임</Label>
-        <Input
-          type="text"
-          id="nickname"
-          value={watch("nickname")}
-          isError={!!errors.nickname}
-          placeholder="닉네임을 입력하세요"
-          {...register("nickname", {
-            required: "닉네임을 입력해주세요",
-            pattern: {
-              value: /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{3,10}$/,
-              message: "닉네임은 3-10글자의 영어,숫자,한글로 입력해주세요.",
-            },
-          })}
-        />
-        <ErrorMsg>
-          {errors.nickname &&
-            "닉네임은 3-10글자의 영어,숫자,한글로 입력해주세요."}
-        </ErrorMsg>
-      </RegisterInput>
+      <FormInput
+        type="text"
+        id="userId"
+        name="아이디"
+        isError={!!errors.userId}
+        placeholder="아이디를 입력하세요"
+        register={register("userId", {
+          required: "아이디를 입력해주세요",
+          pattern: {
+            value: /^(?=.*[a-z0-9])[a-z0-9]{3,10}$/,
+            message: "아이디는 3-10글자의 영어와 숫자로 입력해주세요.",
+          },
+        })}
+      />
+      <ErrorMsg>
+        {errors.userId && "아이디는 3-10글자의 영어와 숫자로 입력해주세요."}
+      </ErrorMsg>
+      <FormInput
+        type="password"
+        id="password"
+        name="비밀번호"
+        isError={!!errors.password}
+        placeholder="비밀번호를 입력하세요"
+        register={register("password", {
+          required: "비밀번호를 입력해주세요",
+          pattern: {
+            value: /^[a-zA-Z0-9!@#$%^&*()._-]{3,10}$/,
+            message: "비밀번호는 3-10글자로 입력해주세요.",
+          },
+        })}
+      />
+      <ErrorMsg>
+        {errors.password && "비밀번호는 3-10글자로 입력해주세요."}
+      </ErrorMsg>
+      <FormInput
+        type="text"
+        id="nickname"
+        name="닉네임"
+        isError={!!errors.nickname}
+        placeholder="닉네임을 입력하세요"
+        register={register("nickname", {
+          required: "닉네임을 입력해주세요",
+          pattern: {
+            value: /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{3,10}$/,
+            message: "닉네임은 3-10글자의 영어,숫자,한글로 입력해주세요.",
+          },
+        })}
+      />
+      <ErrorMsg>
+        {errors.nickname &&
+          "닉네임은 3-10글자의 영어,숫자,한글로 입력해주세요."}
+      </ErrorMsg>
       <Button disabled={Object.keys(errors).length !== 0}>회원가입</Button>
     </Root>
   );
@@ -115,47 +107,27 @@ const Root = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   width: 100%;
   height: 100%;
+
+  & > div > input {
+    margin-bottom: 3px;
+  }
 `;
 
 const Title = styled.h2`
   ${({ theme }) => css`
+    align-self: center;
     margin-bottom: 20px;
     font: ${theme.font.bold_16};
-  `}
-`;
-
-const RegisterInput = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  width: 100%;
-  width: 280px;
-`;
-
-const Label = styled.label`
-  ${({ theme }) => css`
-    margin-bottom: 5px;
-    font: ${theme.font.regular_12};
-  `}
-`;
-
-const Input = styled.input<{ isError: boolean }>`
-  ${({ theme, isError }) => css`
-    width: 100%;
-    height: 40px;
-    border: 1px solid ${isError ? theme.color.red_02 : theme.color.gray_01};
-    border-radius: 2px;
-    margin-bottom: ${!isError && "27px"};
   `}
 `;
 
 const ErrorMsg = styled.p`
   ${({ theme }) => css`
     padding: 3px 0;
-    margin-bottom: 6px;
+    margin-bottom: 15px;
     font: ${theme.font.regular_8};
     color: ${theme.color.red_02};
   `}
